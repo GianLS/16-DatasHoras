ddc003f9c8d8ab8a4a4c4d788b10d8ad11ca6c84
Giancarlo Leite <giancarlo.leite@outlook.com>
2022-06-11 21:31:01 -0300

    Commit inicial do projeto

---
+++ src/br/com/glandata/Main.java
@@ -0,0 +1,18 @@
+package br.com.glandata;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Main {
+	public static void main(String[] args) {
+		LocalDate dataAgora = LocalDate.now();
+
+		DateTimeFormatter formataData = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		System.out.println(formataData.format(dataAgora));
+
+		LocalDateTime dataHoraAgora = LocalDateTime.now();
+		DateTimeFormatter formataDataHora = DateTimeFormatter.ofPattern("dd 'de' MMMM 'de' yyyy 'às' HH:mm:ss.ms");
+		System.out.println(formataDataHora.format(dataHoraAgora));
+	}
+}
